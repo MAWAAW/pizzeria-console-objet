4b21f8b3c279d66fc1f7cdd6dfc355e724cd8da0
ETY0005 <ETY0005@DESKTOP-TPSBHAN>
2017-11-13 16:42:47 +0100

    tp pizza

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,10 +1,79 @@
 package fr.pizzeria.console;
 
+import java.util.Scanner;
+import fr.pizzeria.model.*;
+
 public class PizzeriaAdminConsoleApp {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		Pizzaria p = new Pizzaria();
+		
+		int choix = 0;
+	
+		while (choix != 99) {
+			
+			p.menu();
+
+			choix = Integer.parseInt(sc.nextLine());
+
+			switch (choix) {
+
+			case 1:
+				p.affichePizzas();
+				break;
+			case 2:
+				System.out.println("Veuillez saisir le code");
+				String code = sc.nextLine();
+				System.out.println("Veuillez saisir le nom (sans espace)");
+				String nom = sc.nextLine();
+				System.out.println("Veuillez saisir le prix");
+				double prix = Double.parseDouble(sc.nextLine());
+				p.ajoutePizza(code, nom, prix);
+				break;
+			case 3:
+				p.affichePizzas();
+				System.out.println("Veuillez saisir le code de la pizza à modifier");
+				Pizza pizzaAModifier = p.cherchePizza(sc.nextLine());
+				if (pizzaAModifier != null) {
+					System.out.println("Veuillez saisir le code");
+					String code2 = sc.nextLine();
+					System.out.println("Veuillez saisir le nom (sans espace)");
+					String nom2 = sc.nextLine();
+					System.out.println("Veuillez saisir le prix");
+					double prix2 = Double.parseDouble(sc.nextLine());
+					p.modifPizza(pizzaAModifier, code2, nom2, prix2);
+				}
+				else {
+					System.out.println("La pizza n\'existe pas...");
+				}
+				break;
+			case 4:
+				p.affichePizzas();
+				System.out.println("Veuillez saisir le code de la pizza à supprimer");
+				Pizza pizzaASupprimer = p.cherchePizza(sc.nextLine());
+				if (pizzaASupprimer != null) {
+					p.supprimePizza(pizzaASupprimer);
+				}
+				else {
+					System.out.println("La pizza n\'existe pas...");
+				}
+				break;
+			case 99:
+				choix = 99;
+				break;
+			default:
+				System.out.println("Choix invalide...");
+				break;
+			}
+
+		}
+		
+		System.out.println("fin du programme...");
+		
+		sc.close();
 	}
 
 }
---
+++ src/fr/pizzeria/model/Pizza.java
@@ -0,0 +1,59 @@
+package fr.pizzeria.model;
+
+public class Pizza {
+
+	private static int numberOfId = 0;
+	private int id;
+	private String code;
+	private String nom;
+	private double prix;
+	
+	public Pizza(String code, String nom, double prix) {
+		this.id = this.numberOfId;
+		this.numberOfId++;
+		this.code = code;
+		this.nom = nom;
+		this.prix = prix;
+	}
+	
+	
+	public int getId() {
+		
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		
+		this.id = id;
+	}
+	
+	public String getCode() {
+		
+		return this.code;
+	}
+	
+	public void setCode(String code) {
+		
+		this.code = code;
+	}
+	
+	public String getNom() {
+		
+		return this.nom;
+	}
+	
+	public void setNom(String nom) {
+		
+		this.nom = nom;
+	}
+	
+	public double getPrix() {
+		
+		return this.prix;
+	}
+	
+	public void setPrix(double prix) {
+		
+		this.prix = prix;
+	}
+}
